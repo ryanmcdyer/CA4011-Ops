9b9ea9a46e4d6f221d667dbfb5a96585a1b3cb02
Ryan McDyer <ryan.mcdyer2@mail.dcu.ie>
2017-03-15 21:43:33 +0000

    20170315-1

--- Assignment1/Part1.java
+++ Assignment1/Part1.java
@@ -8,6 +8,8 @@ import java.io.PrintWriter;
 import java.time.Instant;*/
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 import java.security.SecureRandom;
@@ -27,6 +29,8 @@ results from your software model. Key measures of performance include
 * The number of replications to be applied should be an input parameter for the simulation system.
 
 */
+
+  static ArrayList<Client> allClients;
 
   public static void main(String[] args) {
 
@@ -59,38 +63,59 @@ results from your software model. Key measures of performance include
     }
 
     int numClients = (int) Math.round((lengthOfSimulation/60.0) * arrivalRate);
-    System.out.println(numClients);
 
-    ArrayList<Client> q = new ArrayList<>();
+
+    populateClients(avgTimeBetweenArrivals, arrivalRate, lengthOfSimulation);
+
+    /*for(Client tmp : allClients) {
+      System.out.println(tmp.getArrivalTime());
+    }*/
+
 
     int i = 0;
-    double t = 30/arrivalRate;
-    double arrivalTime = t;
-    SecureRandom sr = new SecureRandom();
-    while(i < 100) {
-      t += avgTimeBetweenArrivals;
-      arrivalTime = t + (avgTimeBetweenArrivals/2 * getArrivalTimeExp(arrivalRate, sr) * ( sr.nextBoolean() ? 1 : -1 ));
-      //Time
-
-      q.add(new Client((int) arrivalTime));
+    while(i < numSimulations) {
+      runSimulation(lengthOfSimulation);
       i++;
     }
+  }
 
-    for(Client tmp : q) {
-      System.out.println(tmp.getArrivalTime());
+  static void runSimulation(int lengthOfSimulation) {
+
+/*
+- Check if there's a new arrival
+  -> if so, add to queue
+
+- Check if the queue is !empty
+  -> if so, check if a server is free
+    -> if so, make server busy, remove client from queue, set client departureTime
+
+- Check if
+
+*/
+
+
+    LinkedList<Client> q = new LinkedList<>();
+    int time = 0;
+    Client tmp;
+    while(time < lengthOfSimulation) {
+      tmp = newArrival(time);
+      if(tmp != null) {
+        //tmp.setBusy();
+      }
+      time++;
     }
-
   }
 
-  static double getArrivalTimeExp(double arrivalRate, Random rand) {
-    //http://stackoverflow.com/questions/2106503/pseudorandom-number-generator-exponential-distribution
-    /*double d = Math.log(1-rand.nextDouble())/(-arrivalRate);
-    System.out.println("d" + d);
-    return d;*/
-    return Math.log(1-rand.nextDouble())/(-arrivalRate);
+  static Client newArrival(int time) {
+    for(Client c : allClients) {
+      if(c.getArrivalTime() == time) {
+        return c;
+      }
+    }
+    return null;
   }
 
-  static int getPoisson(double lambda) {
+  static int getArrivalTimePoisson(double lambda) {
     //http://stackoverflow.com/questions/1241555/algorithm-to-generate-poisson-and-binomial-random-numbers
     double L = Math.exp(-lambda);
     double p = 1.0;
@@ -103,16 +128,47 @@ results from your software model. Key measures of performance include
 
     return k - 1;
   }
-}
 
+  static double getArrivalTimeExp(double arrivalRate, Random rand) {
+    //http://stackoverflow.com/questions/2106503/pseudorandom-number-generator-exponential-distribution
+    /*double d = Math.log(1-rand.nextDouble())/(-arrivalRate);
+    System.out.println("d" + d);
+    return d;*/
+    return Math.log(1-rand.nextDouble())/(-arrivalRate);
+  }
+
+  static void populateClients(double avgTimeBetweenArrivals, double arrivalRate, int lengthOfSimulation) {
+    allClients = new ArrayList<>();
+    int i = 0;
+    int t = Math.round((float) (30.0/arrivalRate)); //set it to 5 mins
+    int arrivalTime = t;
+    SecureRandom sr = new SecureRandom();
+    while(i < lengthOfSimulation) {
+      t += avgTimeBetweenArrivals;
+      arrivalTime = (int) (0.5 + t + (avgTimeBetweenArrivals/2
+          * getArrivalTimeExp(arrivalRate, sr)
+          * ( sr.nextBoolean() ? 1 : -1 )));
+
+      //System.out.println(t + " " + arrivalTime);
+      //Time
+
+      q.add(new Client(arrivalTime));
+      i++;
+    }
+  }
+}
 
 class Client {
   private int arrivalTime;
   private int startOfServiceTime;
   private int departureTime;
 
+  private boolean isBusy;
+
   public Client(int a0) {
     arrivalTime = a0;
+    isBusy = false;
+    departureTime = -1;
   }
 
   public Client() {
@@ -142,14 +198,40 @@ class Client {
 	public void setStartOfServiceTime(int startOfServiceTime) {
 		this.startOfServiceTime = startOfServiceTime;
 	}
+
+  public boolean hasBeenServed() {
+    if(departureTime != -1)
+      return true;
+    return false;
+  }
+
+  public boolean isBusy() {
+    return isBusy;
+  }
 }
 
 class Server {
-
   private int breakLength;
-  private ArrayList<Integer> breakTimes;
+  private ArrayList<Integer> breakTimes;// = new ArrayList<>();
+  private boolean isBusy;
+  private int serviceTime;//how long it takes this server to serve one client
+
+  public Server(int breakLength0, int[] breakTimes0, int serviceTime0) {
+    breakLength = breakLength0;
+    breakTimes = new ArrayList<>();
+    for(int bt : breakTimes0) {
+      breakTimes.add(bt);
+    }
+    serviceTime = serviceTime0;
+    isBusy = false;
+  }
 
   public Server() {
     ;
   }
+
+  public boolean isBusy() {
+    return isBusy;
+  }
+
 }
