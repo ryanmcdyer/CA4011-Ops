21302654964d84b23126f6b4ab33a982c20a6790
Ryan McDyer <ryan.mcdyer2@mail.dcu.ie>
2017-03-16 13:47:55 +0000

    20170316-1

--- Assignment1/Part1.java
+++ Assignment1/Part1.java
@@ -31,12 +31,14 @@ results from your software model. Key measures of performance include
 */
 
   static ArrayList<Client> allClients;
+  static ArrayList<Server> allServers;
 
   public static void main(String[] args) {
 
     double arrivalRate = 0;
     double serviceRate = 0;
     double avgTimeBetweenArrivals = 60;
+    int serviceTime = -1;
     int numServers = 0;
     int lengthOfSimulation = 0;
     int numSimulations = 1;
@@ -53,6 +55,7 @@ results from your software model. Key measures of performance include
       arrivalRate = Double.parseDouble(c.readLine("Arrival Rate (x per hour): ")); //lambda
       avgTimeBetweenArrivals = 60/arrivalRate;
       serviceRate = Double.parseDouble(c.readLine("Service Rate (x per hour): ")); //mu
+      serviceTime = (int) Math.round(60/serviceRate);
       numServers = Integer.parseInt(c.readLine("Number of servers: "));
       lengthOfSimulation = Integer.parseInt(c.readLine("Length of Simulation (in minutes): "));
       //numSimulations = Integer.parseInt(c.readLine("Number of Simulation: "));
@@ -62,18 +65,17 @@ results from your software model. Key measures of performance include
       ;
     }
 
-    int numClients = (int) Math.round((lengthOfSimulation/60.0) * arrivalRate);
+    int numClients = (int) Math.round((lengthOfSimulation/60.0) * arrivalRate);//round double to long, cast to int
 
 
-    populateClients(avgTimeBetweenArrivals, arrivalRate, lengthOfSimulation);
-
-    /*for(Client tmp : allClients) {
-      System.out.println(tmp.getArrivalTime());
-    }*/
 
 
     int i = 0;
     while(i < numSimulations) {
+      populateClients(avgTimeBetweenArrivals, arrivalRate, lengthOfSimulation);
+
+      populateServers(numServers, serviceRate);
+
       runSimulation(lengthOfSimulation);
       i++;
     }
@@ -85,28 +87,63 @@ results from your software model. Key measures of performance include
 - Check if there's a new arrival
   -> if so, add to queue
 
-- Check if the queue is !empty
-  -> if so, check if a server is free
-    -> if so, make server busy, remove client from queue, set client departureTime
+- Check if a server is busy
+  -> if so, check if time == client's departureTime
+    -> if so, free up the server, set client to served
 
-- Check if
+- Check if the queue is populated
+  -> if so, check if a server is free
+    -> if so, make server busy, remove "oldest" client from queue, set client departureTime
+
+
 
 */
 
 
     LinkedList<Client> q = new LinkedList<>();
     int time = 0;
-    Client tmp;
+    Client tmpCli;
+    Server tmpSer;
+    Server[] busySer;
+
+//- Check if there's a new arrival
+//  -> if so, add to queue
     while(time < lengthOfSimulation) {
-      tmp = newArrival(time);
-      if(tmp != null) {
-        //tmp.setBusy();
+      if(isNewArrival(time)) {
+        tmpCli = getNewArrival(time);
+        q.add(tmpCli);
       }
+
+//- Check if a server is busy
+//  -> if so, check if time == client's departureTime
+//    -> if so, free up the server, set client to served
+      busySer = getBusyServers();
+      for(Server s : busySer) {
+        tmpCli = s.getCurrentClient();
+        if(tmpCli.getDepartureTime() == time) {
+          s.releaseClient();
+        }
+      }
+
+
+
+
       time++;
     }
   }
 
-  static Client newArrival(int time) {
+  static boolean isNewArrival(int time) {
+    for(Client c : allClients) {
+      if(c.getArrivalTime() == time) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static Client getNewArrival(int time) {
+    //Guaranteed to return a Client,
+    //Only called when there is a Client with arrivalTime == time
     for(Client c : allClients) {
       if(c.getArrivalTime() == time) {
         return c;
@@ -149,12 +186,39 @@ results from your software model. Key measures of performance include
           * getArrivalTimeExp(arrivalRate, sr)
           * ( sr.nextBoolean() ? 1 : -1 )));
 
-      //System.out.println(t + " " + arrivalTime);
-      //Time
-
-      q.add(new Client(arrivalTime));
+      allClients.add(new Client(arrivalTime));
       i++;
     }
+  }
+
+  static void populateServers(int numServers, double serviceRate, int[] breakTimes) {
+    allServers = new ArrayList<>();
+    int i = 0;
+
+    while(i < numServers) {
+      allServers.add(null);
+      i++;
+    }
+  }
+
+  static void populateServers(int numServers, double serviceRate) {
+    allServers = new ArrayList<>();
+    int i = 0;
+
+    while(i < numServers) {
+      allServers.add(new Server());
+    }
+  }
+
+  static Server[] getBusyServers() {
+    LinkedList<Server> list = new LinkedList<>();
+    for(Server s : allServers) {
+      if(s.isBusy()) {
+        list.add(s);
+      }
+    }
+    Server[] arr = list.stream().toArray(Server[]::new); //Java 8 streams
+    return arr;
   }
 }
 
@@ -208,6 +272,10 @@ class Client {
   public boolean isBusy() {
     return isBusy;
   }
+
+  public void setBusy(boolean b) {
+    isBusy = b;
+  }
 }
 
 class Server {
@@ -215,10 +283,11 @@ class Server {
   private ArrayList<Integer> breakTimes;// = new ArrayList<>();
   private boolean isBusy;
   private int serviceTime;//how long it takes this server to serve one client
+  private Client currentClient;
 
   public Server(int breakLength0, int[] breakTimes0, int serviceTime0) {
     breakLength = breakLength0;
-    breakTimes = new ArrayList<>();
+    breakTimes   = new ArrayList<>();
     for(int bt : breakTimes0) {
       breakTimes.add(bt);
     }
@@ -230,8 +299,29 @@ class Server {
     ;
   }
 
+  public void giveClient(Client c, int time) {
+    currentClient = c;
+    currentClient.setDepartureTime(time + serviceTime);
+    currentClient.setStartOfServiceTime(time);
+    currentClient.setBusy(true);
+  }
+
+  public void releaseClient() {
+    currentClient.setBusy(false);
+    this.setBusy(false);
+    //if(breakTimes.contains(currenttime))
+  }
+
+  public Client getCurrentClient() {
+    return currentClient;
+  }
+
   public boolean isBusy() {
     return isBusy;
+  }
+
+  public void setBusy(boolean b) {
+    isBusy = b;
   }
 
 }
